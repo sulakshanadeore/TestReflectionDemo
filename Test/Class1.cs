9a759fd16639f8b1d20dcd0779e1ddc810dd544a
deoresulakshana <deoresulakshana@gmail.com>
2024-11-13 15:45:33 +0530

    Add project files.

---
+++ Test/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Calculator
+    {
+        public Calculator() 
+        {
+        }
+        
+        private double _number;
+        public double Number { get { return _number; } set { _number = value; } }
+        public void Clear()
+        {
+            _number = 0;
+        }
+        public double Add(double number)
+        {
+            double ans = number + number;
+            return ans;
+        }
+        private static double _pi = 3.14d;
+        public static double Pi
+        {
+            get { return _pi; }
+        }
+
+    }
+
+}
